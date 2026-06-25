8380ed2251ae02d0be4336a0e34e8250644ae1fb
Dan Mills <code@sandmill.org>
2026-06-25 15:16:29 +0200

    feat(broker): landing page at / with 'clear cached identities' button

--- browserid-broker/src/routes/mod.rs
+++ browserid-broker/src/routes/mod.rs
@@ -92,6 +92,8 @@ where
         .route_service("/communication_iframe/start.js", ServeFile::new(format!("{}/communication_iframe/start.js", static_path)))
         // SBO signer popup (first-party broker window for cross-site typed signing)
         .route_service("/sign", ServeFile::new(format!("{}/sign.html", static_path)))
+        // Landing page at the root.
+        .route_service("/", ServeFile::new(format!("{}/index.html", static_path)))
         // Serve static files (dialog, CSS, JS)
         .nest_service("/dialog", ServeDir::new(static_path))
         .layer(CookieManagerLayer::new())
